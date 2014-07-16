4c261e406aed7149c56ed054aa8ffab79d83ee28
Vinicius Vollrath <vinicius@terradigital.com.br>
2014-07-16 14:01:24 -0300

    Mudanças locais

--- bp_content/themes/default/config/boilerplate.py
+++ bp_content/themes/default/config/boilerplate.py
@@ -5,35 +5,33 @@ config = {
     'environment': "boilerplate",
 
     # contact page email settings
-    'contact_sender': "appengine@beecoss.com",
-    'contact_recipient': "appengine@beecoss.com",
+    'contact_sender': "mail@terradigital.com.br",
+    'contact_recipient': "vinicius@terradigital.com.br",
 
     'send_mail_developer': False,
 
     # fellas' list
     'developers': (
-        ('GAE Developer', 'gae-developer2014@beecoss.com'),
+        ('Vinicius Vollrath', 'vinicius@terradigital.com.br'),
     ),
 
     # It is just an example to fill out this value
     'google_analytics_code': """
             <script>
-            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-            })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+                (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+                })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-            ga('create', 'UA-47489500-1', 'auto', {'allowLinker': true});
-            ga('require', 'linker');
-            ga('linker:autoLink', ['beecoss.com', 'blog.beecoss.com', 'appengine.beecoss.com']);
-            ga('send', 'pageview');
+                ga('create', 'UA-2238493-1', 'terradigital.com.br');
+                ga('send', 'pageview');
             </script>
         """,
 
     # Password AES Encryption Parameters
     # aes_key must be only 16 (*AES-128*), 24 (*AES-192*), or 32 (*AES-256*) bytes (characters) long.
-    'aes_key': "9c20576a4330bbe719b23ac8bf3bb8a1",
-    'salt': "RdbkETeF$<^>%%X^8|e[9td62`dobFL[V&F&**@`UP6vqjGL,>v+k@ma^zd6WdG0;H>o-SGG9ynk",
+    'aes_key': "9c20276a4330bbe719b23ac8bf3bb8a1",
+    'salt': "RdbkETwF$<^>%%X^8|e[9td62`dobFL[V&F&**@`UP6vqjGL,>v+k@ma^zd6WdG0;H>o-SGG9ynk",
 
     # get your own consumer key and consumer secret by registering at https://dev.twitter.com/apps
     # callback url must be: http://[YOUR DOMAIN]/login/twitter/complete
